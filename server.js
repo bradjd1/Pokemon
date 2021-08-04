4a1512b941fec0b581000dcc56ddeced589e7f8c
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-04 14:57:52 -0500

    Commit 2-Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -2,6 +2,38 @@ const express = require('express');
 
 const app = express();  //create an instance of express called app
 
+//create a pokemon array
+const pokemon = [
+    {
+      name: "Bulbasaur",
+      img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"
+    },
+    {
+      name: "Ivysaur",
+      img: "http://img.pokemondb.net/artwork/ivysaur.jpg"
+    },
+    {
+      name: "Venusaur",
+      img: "http://img.pokemondb.net/artwork/venusaur.jpg"
+    },
+    {
+      name: "Charmander",
+      img: "http://img.pokemondb.net/artwork/charmander.jpg"
+    },
+    {
+      name: "Charizard",
+      img: "http://img.pokemondb.net/artwork/charizard.jpg"
+    },
+    {
+      name: "Squirtle",
+      img: "http://img.pokemondb.net/artwork/squirtle.jpg"
+    },
+    {
+      name: "Wartortle",
+      img: "http://img.pokemondb.net/artwork/wartortle.jpg"
+    }
+  ];
+
 app.listen(3000, ()=>{
     console.log("I am listening");
 });
