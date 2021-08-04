daec086ec5ba665e1c914c74e5420a500c974949
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-04 15:05:23 -0500

    Commit 3 - added GET APIs

--- server.js
+++ server.js
@@ -34,6 +34,14 @@ const pokemon = [
     }
   ];
 
+app.get('/pokemon',(req,res) => {
+    res.send(pokemon);
+})
+
+app.get('/pokemon/:index',(req,res) => {
+    res.send(pokemon[req.params.index]);
+});
+
 app.listen(3000, ()=>{
     console.log("I am listening");
 });
