9fada81a707835c04dac4081475f0edfb3b1f25a
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-06 14:42:18 -0500

    added controller,router and converted to use the router

---
+++ controllers/pokemonController.js
@@ -0,0 +1,45 @@
+//importing express to file
+const express = require('express');
+//instantiating our router
+const router = express.Router();
+const pokemon = require('../models/pokemon')    //need to make it available to the router
+
+router.get('/', (req, res) => {  
+    res.render('index.ejs', {pokemon: pokemon});
+});
+
+router.get("/new", (req, res) => {
+    res.render('new.ejs');
+})
+
+router.get("/:index/edit", (req,res) => {
+    res.render("edit.ejs", {
+        pokemon: pokemon[req.params.index],index: req.params.index
+    })
+})
+
+router.get('/:arrayIndex', (req, res) => {
+    //    res.send(pokemon[req.params.index]);
+    res.render('show.ejs', {
+        pokemon: pokemon[req.params.arrayIndex],
+        arrIndex: req.params.arrayIndex
+    })
+});
+
+router.post("/new", (req, res) => {
+    pokemon.push(req.body);
+    res.redirect('/pokemon');
+})
+
+router.put('/:index', (req,res) => {
+    pokemon[req.params.index] = req.body;
+    res.redirect('/pokemon');
+})
+
+router.delete('/:index', (req,res) => {
+    pokemon.splice(req.params.index,1);
+    res.redirect('/pokemon');
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,36 +4,36 @@ const app = express();  //create an instance of express called app
 const methodOverride = require('method-override');  //used for form to be able to edit and delete
 
 //create a pokemon array
-const pokemon = [
-    {
-        name: "Bulbasaur",
-        img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"
-    },
-    {
-        name: "Ivysaur",
-        img: "http://img.pokemondb.net/artwork/ivysaur.jpg"
-    },
-    {
-        name: "Venusaur",
-        img: "http://img.pokemondb.net/artwork/venusaur.jpg"
-    },
-    {
-        name: "Charmander",
-        img: "http://img.pokemondb.net/artwork/charmander.jpg"
-    },
-    {
-        name: "Charizard",
-        img: "http://img.pokemondb.net/artwork/charizard.jpg"
-    },
-    {
-        name: "Squirtle",
-        img: "http://img.pokemondb.net/artwork/squirtle.jpg"
-    },
-    {
-        name: "Wartortle",
-        img: "http://img.pokemondb.net/artwork/wartortle.jpg"
-    }
-];
+// const pokemon = [
+//     {
+//         name: "Bulbasaur",
+//         img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"
+//     },
+//     {
+//         name: "Ivysaur",
+//         img: "http://img.pokemondb.net/artwork/ivysaur.jpg"
+//     },
+//     {
+//         name: "Venusaur",
+//         img: "http://img.pokemondb.net/artwork/venusaur.jpg"
+//     },
+//     {
+//         name: "Charmander",
+//         img: "http://img.pokemondb.net/artwork/charmander.jpg"
+//     },
+//     {
+//         name: "Charizard",
+//         img: "http://img.pokemondb.net/artwork/charizard.jpg"
+//     },
+//     {
+//         name: "Squirtle",
+//         img: "http://img.pokemondb.net/artwork/squirtle.jpg"
+//     },
+//     {
+//         name: "Wartortle",
+//         img: "http://img.pokemondb.net/artwork/wartortle.jpg"
+//     }
+// ];
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));  //this gives you the body of the request in a nice format, can then console log it to see input
@@ -48,48 +48,45 @@ app.use((req, res, next) => {      //runs on all routes so put it at top
 })
 
 app.use(methodOverride('_method'));
-// app.get('/pokemon',(req,res) => {
-//     res.send(pokemon);
+app.use('/pokemon', require('./controllers/pokemonController'));
 
+// app.get('/pokemon/', (req, res) => {
+//     //    res.send(pokemon[req.params.index]);
+//     res.render('index.ejs', { pokemon: pokemon });
+// });
+
+// app.get("/pokemon/new", (req, res) => {
+//     res.render('new.ejs');
 // })
 
-app.get('/pokemon/', (req, res) => {
-    //    res.send(pokemon[req.params.index]);
-    res.render('index.ejs', { pokemon: pokemon });
-});
+// app.get("/pokemon/:index/edit", (req,res) => {
+//     res.render("edit.ejs", {
+//         pokemon: pokemon[req.params.index],index: req.params.index
+//     })
+// })
 
-app.get("/pokemon/new", (req, res) => {
-    res.render('new.ejs');
-})
+// app.get('/pokemon/:arrayIndex', (req, res) => {
+//     //    res.send(pokemon[req.params.index]);
+//     res.render('show.ejs', {
+//         pokemon: pokemon[req.params.arrayIndex],
+//         arrIndex: req.params.arrayIndex
+//     })
+// });
 
-app.get("/pokemon/:index/edit", (req,res) => {
-    res.render("edit.ejs", {
-        pokemon: pokemon[req.params.index],index: req.params.index
-    })
-})
+// app.post("/pokemon/new", (req, res) => {
+//     pokemon.push(req.body);
+//     res.redirect('/pokemon');
+// })
 
-app.get('/pokemon/:arrayIndex', (req, res) => {
-    //    res.send(pokemon[req.params.index]);
-    res.render('show.ejs', {
-        pokemon: pokemon[req.params.arrayIndex],
-        arrIndex: req.params.arrayIndex
-    })
-});
+// app.put('/pokemon/:index', (req,res) => {
+//     pokemon[req.params.index] = req.body;
+//     res.redirect('/pokemon');
+// })
 
-app.post("/pokemon/new", (req, res) => {
-    pokemon.push(req.body);
-    res.redirect('/pokemon');
-})
-
-app.put('/pokemon/:index', (req,res) => {
-    pokemon[req.params.index] = req.body;
-    res.redirect('/pokemon');
-})
-
-app.delete('/pokemon/:index', (req,res) => {
-    pokemon.splice(req.params.index,1);
-    res.redirect('/pokemon');
-})
+// app.delete('/pokemon/:index', (req,res) => {
+//     pokemon.splice(req.params.index,1);
+//     res.redirect('/pokemon');
+// })
 
 app.listen(3000, () => {
     console.log("I am listening");
