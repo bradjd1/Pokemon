3963ea746365f9d8b6fadc5bfb38000e1278fed8
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-09 19:02:53 -0500

    add logic to edit profile and redirect back to profile page

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const users = require("../models/players");
+const players = require("../models/players");
 
 router.get('/', (req,res) => {
     res.render('players/index.ejs');
@@ -10,5 +10,27 @@ router.get('/', (req,res) => {
 router.get('/signup', (req, res) => {    //remember order - we want new to come before fruits/index so we add it up here
     res.render('players/signup.ejs');
 })
+
+router.get('/profile/:index/edit',(req,res) => {
+    console.log('in edit')
+    res.render('players/editProfile.ejs',{
+        player: players[req.params.index],
+        index: req.params.index
+    })
+})
+
+router.get('/profile/:index',(req,res) => {
+    res.render('players/profile.ejs',{
+        player: players[req.params.index],
+        index: req.params.index
+    })
+})
+
+router.post("/", (req, res) => {
+    players.push(req.body);
+    let index = players.length - 1
+    res.redirect(`/players/profile/${index}`);   //redirect to the fruits get
+})
+
 
 module.exports = router;
