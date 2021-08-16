9a2a73297c64f9874f983dd7ae9e2f9d79f70c10
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-16 15:34:57 -0500

    Comment out console logs and add additional navication buttons to various screens

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -21,7 +21,7 @@ router.put('/profile/:id', (req, res) => {
         where: { id: req.params.id },
         returning: true,
     }).then((player) => {
-        console.log('in put update player', req.body, req.params.id)
+        // console.log('in put update player', req.body, req.params.id)
         res.redirect(`/players/profile/${req.params.id}`);
     });
 })
@@ -33,9 +33,8 @@ router.put('/profile/:id', (req, res) => {
 
 //login
 router.post('/login', (req, res) => {
-    console.log('login', req.body);
     Player.findOne({ where: { username: req.body.username, password: req.body.password } }).then((players) => {
-        console.log('login user is ', players);
+        // console.log('login user is ', players);
         res.redirect(`profile/${players.id}/`)
     });
     //let userId = User.findAll({ where: {username: req.body.username, password: req.body.password}});
@@ -49,9 +48,9 @@ router.post('/login', (req, res) => {
 // })
 // edit page
 router.get('/profile/:id/edit', (req, res) => {
-    console.log('in profile id edit1', req.params.id);
+    // console.log('in profile id edit1', req.params.id);
     Player.findByPk(req.params.id).then((player) => {
-        console.log('in profile id edit2', player)
+        // console.log('in profile id edit2', player)
         res.render('players/editProfile.ejs', {
             player: player,
             id: req.params.id
@@ -68,9 +67,9 @@ router.get('/profile/:id/edit', (req, res) => {
 // })
 //display page after login
 router.get('/profile/:id', (req, res) => {
-    console.log('**** in get proflie id');
+    // console.log('**** in get proflie id');
     Player.findByPk(req.params.id).then((player) => {
-        console.log('********  in get profiles id', req.params.id)
+        // console.log('********  in get profiles id', req.params.id)
         res.render('players/profile.ejs', {
             player: player,
             id: req.params.id
@@ -84,14 +83,14 @@ router.get('/profile/:id', (req, res) => {
 //     res.redirect(`/players/profile/${index}`);
 // })
 router.post("/", (req, res) => {
-    console.log('in create',req.body,req.params.id);
+    // console.log('in create',req.body,req.params.id);
     Player.create(req.body).then ((newPlayer) => {
         res.redirect(`/players/profile/${newPlayer.id}`);
     });
 });
 
 router.delete('/profile/:id', (req, res) => {
-    console.log('in delete',)
+    // console.log('in delete',)
     Player.destroy({ where: { id: req.params.id } });    //splice(place in array to start deleting, how many to remove)
     res.redirect('/players');     //redirect back to players homepage
 });
