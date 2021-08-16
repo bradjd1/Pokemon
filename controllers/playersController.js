27fb44343db35495e5d26e3061b71315cb367ee9
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-16 15:18:40 -0500

    added controllers for Players.  Everything is working now

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const players = require("../players");
+//const players = require("../players");
+const Player = require("../models").Player;
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.render('players/index.ejs');
 })
 
@@ -11,38 +12,89 @@ router.get('/signup', (req, res) => {
     res.render('players/signup.ejs');
 })
 
-router.get('/login', (req,res) => {
+router.get('/login', (req, res) => {
     res.render('players/login.ejs');
 })
 
+router.put('/profile/:id', (req, res) => {
+    Player.update(req.body, {
+        where: { id: req.params.id },
+        returning: true,
+    }).then((player) => {
+        console.log('in put update player', req.body, req.params.id)
+        res.redirect(`/players/profile/${req.params.id}`);
+    });
+})
+
+// router.post('/login', (req, res) => {
+//     let userIndex = players.findIndex(players => players.username == req.body.username && players.password == req.body.password);
+//     res.redirect(`profile/${userIndex}`)
+// })
+
+//login
 router.post('/login', (req, res) => {
-    let userIndex = players.findIndex(players => players.username == req.body.username && players.password == req.body.password);
-    res.redirect(`profile/${userIndex}`)
+    console.log('login', req.body);
+    Player.findOne({ where: { username: req.body.username, password: req.body.password } }).then((players) => {
+        console.log('login user is ', players);
+        res.redirect(`profile/${players.id}/`)
+    });
+    //let userId = User.findAll({ where: {username: req.body.username, password: req.body.password}});
 })
 
-router.get('/profile/:index/edit',(req,res) => {
-    res.render('players/editProfile.ejs',{
-        player: players[req.params.index],
-        index: req.params.index
+// router.get('/profile/:index/edit',(req,res) => {
+//     res.render('players/editProfile.ejs',{
+//         player: players[req.params.index],
+//         index: req.params.index
+//     })
+// })
+// edit page
+router.get('/profile/:id/edit', (req, res) => {
+    console.log('in profile id edit1', req.params.id);
+    Player.findByPk(req.params.id).then((player) => {
+        console.log('in profile id edit2', player)
+        res.render('players/editProfile.ejs', {
+            player: player,
+            id: req.params.id
+        });
     })
 })
 
-router.get('/profile/:index',(req,res) => {
-    res.render('players/profile.ejs',{
-        player: players[req.params.index],
-        index: req.params.index
-    })
-})
+//display page after login
+// router.get('/profile/:index',(req,res) => {
+//     res.render('players/profile.ejs',{
+//         player: players[req.params.index],
+//         index: req.params.index
+//     })
+// })
+//display page after login
+router.get('/profile/:id', (req, res) => {
+    console.log('**** in get proflie id');
+    Player.findByPk(req.params.id).then((player) => {
+        console.log('********  in get profiles id', req.params.id)
+        res.render('players/profile.ejs', {
+            player: player,
+            id: req.params.id
+        });
+    });
+});
 
+// router.post("/", (req, res) => {
+//     players.push(req.body);
+//     let index = players.length - 1
+//     res.redirect(`/players/profile/${index}`);
+// })
 router.post("/", (req, res) => {
-    players.push(req.body);
-    let index = players.length - 1
-    res.redirect(`/players/profile/${index}`);
-})
+    console.log('in create',req.body,req.params.id);
+    Player.create(req.body).then ((newPlayer) => {
+        res.redirect(`/players/profile/${newPlayer.id}`);
+    });
+});
 
-router.delete('/profiles/:index',(req,res) => {
-    players.splice(req.params.index, 1);    //splice(place in array to start deleting, how many to remove)
+router.delete('/profile/:id', (req, res) => {
+    console.log('in delete',)
+    Player.destroy({ where: { id: req.params.id } });    //splice(place in array to start deleting, how many to remove)
     res.redirect('/players');     //redirect back to players homepage
-})
+});
+
 
 module.exports = router;
